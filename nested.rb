8452c7a72d66225014f68411adaab9a9e9a0225a
Alisa McCormick <alisa.cookie.mccormick@gmail.com>
2018-10-15 17:42:42 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -19,8 +19,6 @@ programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-
 	programmer_hash =
 		{
 			:grace_hopper => {
@@ -83,6 +81,7 @@ def adding_matz
         }
      }
 
+programmer_hash[:yukihiro_matsumoto][:known_for["Ruby"][:languages]["LISP", "C"]]
 
 end
 
