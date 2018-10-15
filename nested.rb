5956477447c83af612482f6285402b06507b1502
Alisa McCormick <alisa.cookie.mccormick@gmail.com>
2018-10-15 17:35:57 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -22,20 +22,20 @@ def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 
 	programmer_hash =
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
+		{
+			:grace_hopper => {
+    		:known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+		}
 
 programmer_hash[:alan_kay][:known_for]
 end
@@ -56,6 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
