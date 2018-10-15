fadfb8c8571a26acfa26b349ef6218445605a9d2
Alisa McCormick <alisa.cookie.mccormick@gmail.com>
2018-10-15 17:39:36 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -57,8 +57,8 @@ def dennis_ritchies_language
         }
      }
 
-language = programmer_hash[:dennis_ritchie][:languages]
-language.split("")
+"#{language}" = programmer_hash[:dennis_ritchie][:languages]
+
 end
 
 def adding_matz
