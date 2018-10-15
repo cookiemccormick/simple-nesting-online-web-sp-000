1e72f4a53758f571db1b9e05d756e3c2963897df
Alisa McCormick <alisa.cookie.mccormick@gmail.com>
2018-10-15 17:47:22 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -67,19 +67,19 @@ def adding_matz
 
 	programmer_hash =
  		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+    }
 
 programmer_hash[:yukihiro_matsumoto] = {
 	known_for: "Ruby",
