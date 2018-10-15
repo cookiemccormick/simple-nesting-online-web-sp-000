bac6c78673e3096b812a3e4274b495802549c18f
Alisa McCormick <alisa.cookie.mccormick@gmail.com>
2018-10-15 17:33:59 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -1,19 +1,19 @@
 def hopper
 	programmer_hash =
  		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
+			:grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+    	},
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+    }
 
 programmer_hash[:grace_hopper]
 end
@@ -36,6 +36,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+programmer_hash[alan_kay]		 
 end
 
 def dennis_ritchies_language
